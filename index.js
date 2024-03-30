c533bd2c8d7f823f4e074dd89987615948efe2f8
Mansi-Raj <mansi.raj.jnv@gmail.com>
2024-03-30 11:36:56 +0530

    make changes

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ console.log("Welcome to Spotify");
 
 // Initialize the Variables
 let songIndex=0;
-let audioElement = new Audio("./assests/songs/Apna Bana Le - Bhediya 320 Kbps.mp3");
+let audioElement = new Audio("songs/Apna Bana Le - Bhediya 320 Kbps.mp3");
 let masterPlay=document.getElementById('masterPlay');
 let myProgressBar=document.getElementById('myProgressBar');
+let songItems = Array.from(document.getElementsByClassName('songItems'));
 let songs=[
-    {songName: "Apna Bana Le", filePath:"./assests/songs/Apna Bana Le - Bhediya 320 Kbps.mp3", coverPath: "./assests/covers/apna bana le.jpg"},
-    {songName: "Apna Bana Le", filePath:"./assests/songs/Apna Bana Le - Bhediya 320 Kbps.mp3", coverPath: "./assests/covers/apna bana le.jpg"},
-    {songName: "Apna Bana Le", filePath:"./assests/songs/Apna Bana Le - Bhediya 320 Kbps.mp3", coverPath: "./assests/covers/apna bana le.jpg"},
+    {songName: "Apna Bana Le", filePath:"songs/Apna Bana Le - Bhediya 320 Kbps.mp3", coverPath: "cover/apna bana le.jpg", songTime: "04:21"},
+    {songName: "Nazm Nazm", filePath:"songs/Nazm Nazm feat. Ayushmann Khurrana- Bareilly Ki Barfi 320 Kbps.mp3", coverPath: "cover/nazm nazm.jpeg", songTime:"03:14"},
+    {songName: "O Re Piya", filePath:"songs/O Re Piya - Aaja Nachle 320 Kbps.mp3 ", coverPath: "cover/o re piya.jpeg", songTime: "06:19"},
 ]
 
 
@@ -24,6 +25,12 @@ masterPlay.addEventListener('click',()=>{
         masterPlay.classList.add('fa-circle-play');
     }
 })
+
+songItems.forEach((element, i)=>{
+    element.getElementsByTagName("img")[0].src=songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText=songs[i].songName;
+    element.getElementsByClassName("timeStamp")[0].innerText=songs[i].songTime;
+})
 //handle song play
 
 //listen songs
@@ -36,3 +43,4 @@ audioElement.addEventListener('timeupdate', ()=>{
 myProgressBar.addEventListener('change', ()=>{
     audioElement.currentTime=myProgressBar.value*audioElement.duration/100;
 })
+
